acb5169a51a6ddf2523a9bfa8d03a5f9a9e0d3d4
HP <vladyslav.sovpenko@gmail.com>
2024-09-02 21:25:20 +0300

    add user

--- app/core/tests.py
+++ app/core/tests.py
@@ -8,6 +8,8 @@ from psycopg2 import OperationalError as Psycopg2OpError
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
+from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 
 @patch('core.management.commands.wait_for_db.Command.check')
@@ -32,3 +34,44 @@ class CommandTests(SimpleTestCase):
 
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
+
+
+"""
+Test for models
+"""
+
+
+class ModelTests(TestCase):
+    """Test models."""
+
+    def test_create_user_with_email_successful(self):
+        email = 'test@example.com'
+        password = 'test'
+        user = get_user_model().objects.create_user(
+            email=email, password=password)
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_create_user_with_normalize_email(self):
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+        for email, normalized_email in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, normalized_email)
+
+    def test_create_user_without_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'sample123')
+
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            "test.example.com", '123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
